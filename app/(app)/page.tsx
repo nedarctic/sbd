6eff28bbd9c64e494d1c139b5e79e521f09ffa32
justkimtai <justkimtai@gmail.com>
2026-01-27 19:46:58 +0300

    new route group for non-dashboard routes

---
+++ app/(app)/page.tsx
@@ -0,0 +1,25 @@
+
+import { Hero } from "@/components/hero";
+import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
+import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
+import { hasEnvVars } from "@/lib/utils";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        <Header />
+        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+          <Hero />
+          <main className="flex-1 flex flex-col gap-6 px-4">
+            <h2 className="font-medium text-xl mb-4">Next steps</h2>
+            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          </main>
+        </div>
+        <Footer />
+      </div>
+    </main>
+  );
+}
